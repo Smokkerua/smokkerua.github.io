160430c5e40500c98c2e518f24bf131ac6d2d7f6
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2019-07-10 17:00:22 +0300

    clear commit

--- js/script.js
+++ js/script.js
@@ -112,10 +112,6 @@ $(function() {
                 dots: true,
             });
 
-        /* animation */
-        new WOW().init();
-
-
     /* E-mail */
     $("form").submit(function() { //Change
         var th = $(this);
