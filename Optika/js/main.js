d11822be87b1e49136b8cce30c710a5686e03745
Smokkerua <kynpan3@gmail.com>
2019-11-14 17:26:24 +0200

    create new block project

--- Optika/js/main.js
+++ Optika/js/main.js
@@ -72,9 +72,16 @@ $(document).on('ready', function () {
                 breakpoint: 768
                 , settings: {
                     slidesToShow: 1
+                    
                 , }
 	    }
     ]
+    });
+    $('.reviews-slider').slick({
+        slidesToShow: 1
+        , slidesToScroll: 1
+        , arrows: true
+        , dots: true
     });
     
     
@@ -104,6 +111,53 @@ $(document).on('ready', function () {
         };
     });
     
-    
-    
 });
+    // email
+    $(".form_callback, .form").submit(function () {
+        $.ajax({
+            type: "POST"
+            , url: "./mail.php"
+            , data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("#form").trigger("reset");
+            document.getElementById('modalForm1').style.display = 'none';
+            document.getElementById('modalForm2').style.display = 'none';
+            document.getElementById('modalForm3').style.display = 'none';
+            document.getElementById('modalForm4').style.display = 'none';
+            document.getElementById('modalForm5').style.display = 'none';
+            document.getElementById('modalForm6').style.display = 'none';
+            document.getElementById('modalForm7').style.display = 'none';
+            $("#thanks").show('slow'); setTimeout(function() { $("#thanks").hide('slow'); }, 2000);
+            visible = false;
+        });
+        return false;
+    });
+    var showed1 = function (state) {
+        document.getElementById('modalForm1').style.display = state;
+        document.getElementById('filter_modal1').style.display = state;
+    }
+    var showed2 = function (state) {
+        document.getElementById('modalForm2').style.display = state;
+        document.getElementById('filter_modal2').style.display = state;
+    }
+    var showed3 = function (state) {
+        document.getElementById('modalForm3').style.display = state;
+        document.getElementById('filter_modal3').style.display = state;
+    }
+    var showed4 = function (state) {
+        document.getElementById('modalForm4').style.display = state;
+        document.getElementById('filter_modal4').style.display = state;
+    }
+    var showed5 = function (state) {
+        document.getElementById('modalForm5').style.display = state;
+        document.getElementById('filter_modal5').style.display = state;
+    }
+    var showed6 = function (state) {
+        document.getElementById('modalForm6').style.display = state;
+        document.getElementById('filter_modal6').style.display = state;
+    }
+    var showed7 = function (state) {
+        document.getElementById('modalForm7').style.display = state;
+        document.getElementById('filter_modal7').style.display = state;
+    }
