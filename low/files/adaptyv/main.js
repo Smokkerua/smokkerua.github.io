b37fc97524da48e4d852714cd0d4764595bc264b
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-08-02 22:34:53 +0300

    low

---
+++ low/files/adaptyv/main.js
@@ -0,0 +1,34 @@
+ $(document).on('ready', function () {
+     $('.slider1').slick({
+         slidesToShow: 1
+         , slidesToScroll: 1
+         , autoplay: true
+         , dots: true
+         , autoplaySpeed: 4000
+         , arrows: true
+     });
+      $('.slider2').slick({
+         slidesToShow: 1
+         , slidesToScroll: 1
+         , autoplay: true
+         , dots: true
+         , autoplaySpeed: 4000
+         , arrows: true
+     });
+    $('.slider3').slick({
+         slidesToShow: 1
+         , slidesToScroll: 1
+         , autoplay: true
+         , dots: true
+         , autoplaySpeed: 4000
+         , arrows: true
+     });
+    $('.slider4').slick({
+         slidesToShow: 1
+         , slidesToScroll: 1
+         , autoplay: true
+         , dots: true
+         , autoplaySpeed: 4000
+         , arrows: true
+     });
+ });
