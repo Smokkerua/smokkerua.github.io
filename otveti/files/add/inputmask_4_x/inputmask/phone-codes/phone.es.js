d3506471b1ab304099bcff7cd341ae193abad292
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-08-04 15:36:20 +0300

    index

---
+++ otveti/files/add/inputmask_4_x/inputmask/phone-codes/phone.es.js
@@ -0,0 +1,27 @@
+/*!
+* phone-codes/phone.min.js
+* https://github.com/RobinHerbots/Inputmask
+* Copyright (c) 2010 - 2017 Robin Herbots
+* Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+* Version: 4.0.0-52
+*/
+
+!function (c) {
+    "function" == typeof define && define.amd ? define(["../inputmask"], c) : "object" == typeof exports ? module.exports = c(require("../inputmask")) : c(window.Inputmask)
+}(function (c) {
+    return c.extendAliases({
+        ourphone: {
+            alias: "abstractphone",
+            phoneCodes: [
+                {
+                    mask: "+34 #{7,14}",
+                    cc: "ES",
+                    cd: "Spain",
+                    desc_en: "",
+                    name_ru: "Испания",
+                    desc_ru: ""
+                }
+            ]
+        }
+    })
+});
