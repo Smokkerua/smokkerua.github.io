c522118613f3bc5e0f27b284118908a48d0c2bc6
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-10-27 23:50:39 +0200

    watch

---
+++ WhatsApp/js/main.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function () {
+
+    
+    
+    setTimeout(function() {
+        
+       $(".header__loader").addClass("hide");
+       $(".chat__foreword-loader").addClass("hide"); 
+       $(".keyboard").addClass("hide");
+       $(".author").removeClass("hide");
+       $(".chat__foreword-load").removeClass("hide"); 
+       $(".social__input").removeClass("hide");
+       $(".chat__container").removeClass("hide");
+        
+    }, 5000)
+
+     
+});
