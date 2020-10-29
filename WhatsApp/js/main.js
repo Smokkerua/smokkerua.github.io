93471e86c6b09bc8b49dfc7dd1e8ac7799c88a9b
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-10-29 16:40:51 +0200

    3232

--- WhatsApp/js/main.js
+++ WhatsApp/js/main.js
@@ -9,16 +9,34 @@ $(document).on('ready', function () {
        $(".keyboard").addClass("hide");
        $(".author").removeClass("hide");
        $(".chat__foreword-load").removeClass("hide"); 
+        
 
-    }, 5000)
+            //   загрузка чата
+            setTimeout(function() { 
+                $(".online").addClass("hide");
+                $(".write").removeClass("hide");
+                $('.message').first().removeClass("hide");
+                
+                //   цикл вывода сообщений
+                setInterval(function () {
+                    if($('.message').hasClass('hide')) {
+                        $('.chat__container .hide').first().removeClass("hide");
+                    }    
+                }, 2500);
+                
+                //   когда первые три сообщения отобразились
+                setTimeout(function() {                                                 $(".social__input").removeClass("hide");
+                    $(".online").removeClass("hide");
+                    $(".write").addClass("hide");
+                }, 6000)
+                
+            }, 1000)
+
+        
+
+
+    }, 2000)
     
-    //   загрузка чата
-    setTimeout(function() {
-        $(".chat__container").removeClass("hide");
-        $(".social__input").removeClass("hide");
-    }, 8000)
-
-     
     //    Модальное окно
     $(".author-info").on("click", function (event) {
         event.preventDefault();
@@ -30,6 +48,67 @@ $(document).on('ready', function () {
         $(".modal-window").addClass("hide-modal");
     });
     
+    //   подключиться к соц. сети
+    $(".soc-but").on("click", function (event) {
+        event.preventDefault();
+        $(".social__input").addClass("hide");
+        $(".keyboard").removeClass("hide");
+    });
     
 
+butt.onclick = function() {
+    var val = document.getElementById('elem1').value;
+
+var $messagesContainer = $('.chat__container')   
+var $answerContainer = $(`<div class="message user-message">
+    <p class="message-content">${val}</p>
+    <p class="message-time">16:07</p></div>`);
+    
+var $answer = $(`<div class="message">
+    <p class="message-user"><span>Вы</span>
+    ${val}</p>
+    <p class="message-content">Подождите, отвечу на все вопросы после косультации</p>
+    <p class="message-time">16:09</p></div> `);
+        
+    
+    
+$messagesContainer.append($answerContainer);
+    
+setTimeout(() => $messagesContainer.append($answer), 4000); 
+
+  
+};
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
