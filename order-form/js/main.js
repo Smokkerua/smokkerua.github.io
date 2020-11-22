0c66b04461455abfc460949ca42762a65d254c36
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-11-22 20:25:53 +0200

    4343

--- order-form/js/main.js
+++ order-form/js/main.js
@@ -335,20 +335,3 @@ jQuery(($) => {
     });
 });
 
-
-document.ontouchmove = function ( event ) {
-
-    var isTouchMoveAllowed = true, target = event.target;
-
-    while ( target !== null ) {
-        if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
-            isTouchMoveAllowed = false;
-            break;
-        }
-        target = target.parentNode;
-    }
-
-    if ( !isTouchMoveAllowed ) {
-        event.preventDefault();
-    }
-};
