1c3c8191e5085857415a4f5e7a595d1c4b6385c6
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-11-22 20:11:50 +0200

    5454

--- order-form/js/main.js
+++ order-form/js/main.js
@@ -334,3 +334,17 @@ jQuery(($) => {
         }
     });
 });
+
+
+$(function(){
+    if (/iPhone|iPod|iPad/.test(navigator.userAgent))
+        $('iframe').wrap(function(){
+            var $this = $(this);
+            return $('<div />').css({
+                width: $this.attr('width'),
+                height: $this.attr('height'),
+                overflow: 'scroll',
+                '-webkit-overflow-scrolling': 'touch'
+            });
+        });
+})
