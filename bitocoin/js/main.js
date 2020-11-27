3730789410a90b7f9179a5e4ffa9108753d74be3
Smokkerua <52384193+Smokkerua@users.noreply.github.com>
2020-11-27 12:19:57 +0200

    3

---
+++ bitocoin/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    //Скрипт генерации паролей
+    function str_rand() {
+        var result = '';
+        var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+        var max_position = words.length - 1;
+        for (i = 0; i < 6; ++i) {
+            position = Math.floor(Math.random() * max_position);
+            result = result + words.substring(position, position + 1);
+        }
+        return result;
+    }
+    $('.showPassword').click(function () {
+        var inputPsw = $('#password');
+        if (inputPsw.attr('type') == 'password') {
+            event.preventDefault();
+            document.getElementById('password').setAttribute('type', 'text');
+        } else {
+            event.preventDefault();
+            document.getElementById('password').setAttribute('type', 'password');
+        }
+    });
+    $('.generatePassword img').click(function () {
+        event.preventDefault();
+        document.getElementById('password').setAttribute('type', 'text');
+        $('#password').attr('value', str_rand());
+
+    });
+    $('#buttom-form').click(function () {
+        if (document.getElementById('password').value == '') {
+            document.getElementById('password').setAttribute('type', 'text');
+            $('#password').attr('value', str_rand());
+        }
+    });
+});
+
+
+
